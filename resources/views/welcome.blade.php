19170c659ff807ad40b1881baf5c2d3435ee0c65
knopmickael <knopmickael@gmail.com>
2021-03-24 19:21:58 -0300

    Title change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <h1 class="text-3xl font-black text-white border-b-4 border-white pb-4">Welcome to my API</h1>
+    <h1 class="text-3xl font-black text-white border-b-4 border-white pb-4">Welcome to My API</h1>
 
     <p class="text-xl text-secondary">Please check this <a class="font-black hover:text-yellow-400" href="/api">link</a> to access it</p>
 
